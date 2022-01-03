c15c57c0a1da5e9e90ab36bc71060fcf687a377e
Elena <elena.mesic@fer.hr>
2022-01-03 12:39:59 +0100

    Dao changes

--- server/src/main/java/com/example/dao/PantryDao.java
+++ server/src/main/java/com/example/dao/PantryDao.java
@@ -6,11 +6,12 @@ import com.example.model.Pantry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PantryDao extends JpaRepository<Pantry,Integer> {
 
     @Query("SELECT s from smocnica s LEFT OUTER JOIN s.user where s.user.id = :id")
-    public List<Pantry> getForUser(int id);
+    public List<Pantry> getForUser(@Param("id") int id);
 }
